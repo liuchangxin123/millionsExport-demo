8f6f899108e19b253c93f84d9b5ce311313d7a73
pluto <changxin.liu@innodealing.com>
2023-05-10 15:38:07 +0800

    初始化项目

---
+++ src/main/java/com/pluto/millionsExport/MillionsExportApplication.java
@@ -0,0 +1,13 @@
+package com.pluto.millionsExport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillionsExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MillionsExportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluto/millionsExport/MillionsExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluto.millionsExport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillionsExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
